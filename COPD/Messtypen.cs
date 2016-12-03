2c8f3a467d952e5b26084bd3b117e912d12a04e5
Dirk Schumann <Dirk.Schumann@crafting-it.de>
2016-12-03 01:42:19 +0100

    test

--- COPD/Messtypen.cs
+++ COPD/Messtypen.cs
@@ -22,6 +22,7 @@ namespace COPD
     
         public int Id { get; set; }
         public string TypName { get; set; }
+        public Nullable<int> Zeitintervall { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Messdaten> Messdaten { get; set; }
